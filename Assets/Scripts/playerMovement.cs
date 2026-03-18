14c7c7894ee2ba1acb76f5388f2dd06234b3a3bb
Enzo MH <enzomh.web@gmail.com>
2026-03-17 21:34:42 -0300

    Movimentação (funcional)
    Movimentação funcionando, mas não de forma definitiva. Será necessário adicionar uma desaceleração gradual ao trocar a direção do movimento.

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class playerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public Rigidbody2D rb;
+    
+    [Header("Movimentação")]
+    public float moveSpeed = 5f;
+    float horizontalMovement;
+    float verticalMovement;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        rb.linearVelocity = new Vector2(horizontalMovement * moveSpeed, verticalMovement * moveSpeed);
     }
+
+    public void OnMove(InputValue value)
+    {
+        Debug.Log("receba");
+        horizontalMovement = value.Get<Vector2>().x;
+        verticalMovement = value.Get<Vector2>().y;
+    }
+
 }
